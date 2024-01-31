2e300348079fd2164d8096d50673483d60241913
unknown <rikatiara56@gmail.com>
2024-01-31 08:34:25 +0700

    update

---
+++ pkg/who.py
@@ -0,0 +1,28 @@
+import requests as r
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+
+
+
+def scrape_case(url,class_name):
+  data = r.get(url)
+  soup = BeautifulSoup(data.content, 'html.parser')
+  x = soup.find_all("span", class_=class_name)
+  g = x[2].text
+  h = re.findall(r'\d+', g)
+  s_case = ''.join(map(str,h))
+  return s_case
+
+def scrape_death(url,class_name):
+  data = r.get(url)
+  soup = BeautifulSoup(data.content, 'html.parser')
+  x2 = soup.find("span", class_=class_name).text
+  h = re.findall(r'\d+', x2)
+  s_death = ''.join(map(str,h))
+  return s_death
+
+jumlah_positif = str(scrape_case('https://covid19.who.int/','sc-fzoVTD ckBKGO'))
+jumlah_sembuh = '0'
+jumlah_meninggal = str(scrape_death('https://covid19.who.int/','sc-fzoVTD wbXOD'))
